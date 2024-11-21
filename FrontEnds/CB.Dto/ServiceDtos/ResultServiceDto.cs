f047aae37508e4a2b3de746e5cd89c66f69dc2b5
Zehra Seren <fatmazehraseren@gmail.com>
2024-11-22 01:40:34 +0300

    Service part is completed.

---
+++ FrontEnds/CB.Dto/ServiceDtos/ResultServiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CB.Dto.ServiceDtos
+{
+    public class ResultServiceDto
+    {
+        public int serviceId { get; set; }
+        public string title { get; set; }
+        public string? description { get; set; }
+        public string? iconUrl { get; set; }
+    }
+}
