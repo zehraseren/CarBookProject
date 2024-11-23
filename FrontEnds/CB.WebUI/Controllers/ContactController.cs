fe64d67dd68cc461cacec0c47fc5fcac46100bdc
Zehra Seren <fatmazehraseren@gmail.com>
2024-11-23 13:31:40 +0300

    Address part is completed.

---
+++ FrontEnds/CB.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using CB.Dto.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CB.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ContactController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateContactDto ccd)
+        {
+            var client = _httpClientFactory.CreateClient();
+            ccd.SendDate = DateTime.Now;
+            var jsonData = JsonConvert.SerializeObject(ccd);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44347/api/Contacts", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
---
+++ FrontEnds/CB.WebUI/ViewComponents/FooterAddressComponents/_FooterAddressComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using CB.Dto.FooterAddressDtos;
+
+namespace CB.WebUI.ViewComponents.FooterAddressComponents
+{
+    public class _FooterAddressComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44347/api/FooterAddresses");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
