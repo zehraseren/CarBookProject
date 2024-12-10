6f4efe4bed896d692f50f6be5d8445a707d55530
Zehra Seren <fatmazehraseren@gmail.com>
2024-12-10 21:52:27 +0300

    Comment entity added.

--- Core/CB.Domain/Entities/Blog.cs
+++ Core/CB.Domain/Entities/Blog.cs
@@ -12,5 +12,6 @@
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public List<TagCloud> TagClouds { get; set; }
+        public List<Comment> Comments { get; set; }
     }
 }
---
+++ Core/CB.Domain/Entities/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace CB.Domain.Entities
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
