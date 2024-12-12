cc2d43214dd4e01af2f1103c88f5d57c63646618
Zehra Seren <fatmazehraseren@gmail.com>
2024-12-12 15:57:25 +0300

    Revert "Area | AdminBanner part is completed."
    This reverts commit 4836ff56a531610ec0f6cc093a4ca6b529b7b64a.

--- Presentation/CarBookProject.WebApi/Controllers/BannersController.cs
+++ Presentation/CarBookProject.WebApi/Controllers/BannersController.cs
@@ -45,7 +45,7 @@ namespace CB.WebApi.Controllers
             return Ok("Banner bilgisi eklendi.");
         }
 
-        [HttpDelete("{id}")]
+        [HttpDelete]
         public async Task<IActionResult> RemoveBanner(int id)
         {
             await _removeBannerCommandHandler.Handle(new RemoveBannerCommand(id));
