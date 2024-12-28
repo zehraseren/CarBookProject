6ff5191c99d156defd0ed71a6c3265672c0b5c7d
Zehra Seren <fatmazehraseren@gmail.com>
2024-12-28 19:38:59 +0300

    Integrate pivot table data into UI

---
+++ FrontEnds/CB.Dto/CarPricingDtos/ResultCarPricingListWithModelDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CB.Dto.CarPricingDtos
+{
+    public class ResultCarPricingListWithModelDto
+    {
+        public string Model { get; set; }
+        public string coverImageUrl { get; set; }
+        public decimal DailyPrice { get; set; }
+        public decimal WeeklyPrice { get; set; }
+        public decimal MonthlyPrice { get; set; }
+    }
+}
--- FrontEnds/CB.WebUI/Controllers/CarPricingController.cs
+++ FrontEnds/CB.WebUI/Controllers/CarPricingController.cs
@@ -1,13 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CB.Dto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace CB.WebUI.Controllers
 {
     public class CarPricingController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CarPricingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Paketler";
             ViewBag.v2 = "Araç Fiyat Paketleri";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44347/api/CarPricings/GetCarPricingWithTimePeriod");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
